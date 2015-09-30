783988c7774508f92699377149854e265cd975c5
Jackie Aldama <jaldama@bu.edu>
2015-09-30 16:28:38 -0400

    initial commit

---
+++ test/controllers/itinerary_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ItineraryControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/itinerary_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ItineraryHelperTest < ActionView::TestCase
+end
